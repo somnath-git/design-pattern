8391b21bb4208f23535db2bb76c7f96543767c23
Somnath Ghosh <somnathghosh@csod.com>
2024-04-28 15:46:41 +0530

    PrototypeDesign pattern

---
+++ Prototype/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Prototype;
+/*
+ * We no need to assign each property again to create same copy
+ */
+
+
+var rectangle = new RectangleShape();
+rectangle.Width = 10;
+rectangle.Height = 10;
+
+var rectangle2 = rectangle.Clone();
+
+Console.WriteLine("New Rectangle height " + ((RectangleShape)rectangle2).Height);
+
+Console.ReadKey();
---
+++ Prototype/RectangleShape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class RectangleShape : ICloneableShape
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+        public ICloneableShape Clone()
+        {
+            var rectangle = new RectangleShape() { Height = Height, Width = Width };
+            return rectangle;
+        }
+    }
+}
